e97d64c6e499d819eb62293468db3863270262c9
leemhoon00 <leemhoon000@gmail.com>
2024-08-10 00:45:20 +0900

    init

---
+++ src/main.ts
@@ -0,0 +1,39 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: '*',
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+  });
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+      transformOptions: {
+        enableImplicitConversion: true,
+      },
+      enableDebugMessages: true,
+    }),
+  );
+
+  // swagger
+  const config = new DocumentBuilder()
+    .setTitle('to_morrow API')
+    .setDescription('to_morrow API description')
+    .setVersion('1.0')
+    .addTag('to_morrow')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(3000);
+  console.log(`listening on port 3000`);
+}
+bootstrap();
