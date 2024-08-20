5a12410dbd38d461b1807565adabc35041b1b620
leemhoon00 <leemhoon000@gmail.com>
2024-08-21 00:04:41 +0900

    fix: swagger문서 수정

--- src/poem/dto/request/update-tag.dto.ts
+++ src/poem/dto/request/update-tag.dto.ts
@@ -7,19 +7,19 @@ export class UpdateTagDto {
   @IsString()
   readonly title: string;
 
-  @ApiProperty({ enum: themes })
+  @ApiProperty({ isArray: true, enum: themes })
   @IsEnum(themes, { each: true })
   readonly beforeThemes: string[];
 
-  @ApiProperty({ enum: interactions })
+  @ApiProperty({ isArray: true, enum: interactions })
   @IsEnum(interactions, { each: true })
   readonly beforeInteractions: string[];
 
-  @ApiProperty({ enum: themes })
+  @ApiProperty({ isArray: true, enum: themes })
   @IsEnum(themes, { each: true })
   readonly afterThemes: string[];
 
-  @ApiProperty({ enum: interactions })
+  @ApiProperty({ isArray: true, enum: interactions })
   @IsEnum(interactions, { each: true })
   readonly afterInteractions: string[];
 
