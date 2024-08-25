65e4553d068db562728a5f7da4049b9becdd2642
leemhoon00 <leemhoon000@gmail.com>
2024-08-25 19:45:07 +0900

    fix: achievement -> achievements

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -8,48 +8,48 @@ async function main() {
       {
         name: '첫 발자국',
         description: '첫 번째 시를 게시한 경우',
-        icon: process.env.AWS_CLOUDFRONT_URL + '/achievement/achievement1.svg',
+        icon: process.env.AWS_CLOUDFRONT_URL + '/achievements/achievement1.svg',
       },
       {
         name: '목소리의 주인공',
         description: '낭독된 시가 30회 이상 플레이 된 경우',
-        icon: process.env.AWS_CLOUDFRONT_URL + '/achievement/achievement2.svg',
+        icon: process.env.AWS_CLOUDFRONT_URL + '/achievements/achievement2.svg',
       },
       {
         name: '별이 된 시',
         description:
           '작성한 시 중 하나가 독자들에 의해 10회 이상 스크랩 된 경우',
-        icon: process.env.AWS_CLOUDFRONT_URL + '/achievement/achievement3.svg',
+        icon: process.env.AWS_CLOUDFRONT_URL + '/achievements/achievement3.svg',
       },
       {
         name: '매일 그대와',
         description: '10일 연속 접속한 경우',
-        icon: process.env.AWS_CLOUDFRONT_URL + '/achievement/achievement4.svg',
+        icon: process.env.AWS_CLOUDFRONT_URL + '/achievements/achievement4.svg',
       },
       {
         name: '테마 수집가',
         description: '모든 테마 태그를 한번 이상 사용한 경우',
-        icon: process.env.AWS_CLOUDFRONT_URL + '/achievement/achievement5.svg',
+        icon: process.env.AWS_CLOUDFRONT_URL + '/achievements/achievement5.svg',
       },
       {
         name: '상호작용 수집가',
         description: '모든 상호작용 태그를 한번 이상 사용한 경우',
-        icon: process.env.AWS_CLOUDFRONT_URL + '/achievement/achievement6.svg',
+        icon: process.env.AWS_CLOUDFRONT_URL + '/achievements/achievement6.svg',
       },
       {
         name: '새벽감성 작가',
         description: '새벽 2시 이후 작성한 시가 50회 이상 스크랩된 경우',
-        icon: process.env.AWS_CLOUDFRONT_URL + '/achievement/achievement7.svg',
+        icon: process.env.AWS_CLOUDFRONT_URL + '/achievements/achievement7.svg',
       },
       {
         name: '들쑥날쑥',
         description: '모든 기분을 한번 이상 선택한 경우',
-        icon: process.env.AWS_CLOUDFRONT_URL + '/achievement/achievement8.svg',
+        icon: process.env.AWS_CLOUDFRONT_URL + '/achievements/achievement8.svg',
       },
       {
         name: '열번 찍어 넘어간 나무',
         description: '시를 10회 이상 스크랩한 경우',
-        icon: process.env.AWS_CLOUDFRONT_URL + '/achievement/achievement9.svg',
+        icon: process.env.AWS_CLOUDFRONT_URL + '/achievements/achievement9.svg',
       },
     ],
     skipDuplicates: true,
