1747aa1c919eee9878af7d668a0be90e3428fa06
leemhoon00 <leemhoon000@gmail.com>
2024-09-19 20:35:48 +0900

    docs: 글감받기 swagger 작업

--- src/inspiration/inspiration.controller.ts
+++ src/inspiration/inspiration.controller.ts
@@ -38,6 +38,11 @@ export class InspirationController {
       ],
     },
   })
+  @ApiResponse({
+    status: 404,
+    description:
+      'no inspiration - 데이터베이스에 글감이 없어서 나는 에러입니다.',
+  })
   @Get()
   async getOne(
     @Query() { type }: GetInspirationQueryDto,
@@ -62,92 +67,4 @@ export class InspirationController {
       throw e;
     }
   }
-
-  // @ApiOperation({
-  //   summary: '제목 글감 받기 - 하루마다 랜덤으로 바뀜',
-  // })
-  // @ApiResponse({ status: 200, type: TitleDto })
-  // @ApiResponse({
-  //   status: 404,
-  //   description:
-  //     'no inspiration - 데이터베이스에 글감이 없어서 나는 에러입니다.',
-  // })
-  // @Get('title')
-  // async getTitle() {
-  //   try {
-  //     return await this.inspirationService.getTitle();
-  //   } catch (e) {
-  //     if (e instanceof Error) {
-  //       if (e.message === 'no inspiration') {
-  //         throw new HttpException('no inspiration', 404);
-  //       }
-  //     }
-  //     throw e;
-  //   }
-  // }
-
-  // @ApiOperation({
-  //   summary: '단어 글감 받기 - 하루마다 랜덤으로 바뀜',
-  // })
-  // @ApiResponse({ status: 200, type: WordDto })
-  // @ApiResponse({
-  //   status: 404,
-  //   description:
-  //     'no inspiration - 데이터베이스에 글감이 없어서 나는 에러입니다.',
-  // })
-  // @Get('word')
-  // async getWord() {
-  //   try {
-  //     return await this.inspirationService.getWord();
-  //   } catch (e) {
-  //     if (e instanceof Error) {
-  //       if (e.message === 'no inspiration') {
-  //         throw new HttpException('no inspiration', 404);
-  //       }
-  //     }
-  //     throw e;
-  //   }
-  // }
-
-  // @ApiOperation({ summary: '오디오 글감 받기 - 하루마다 랜덤으로 바뀜' })
-  // @ApiResponse({ status: 200, type: AudioDto })
-  // @ApiResponse({
-  //   status: 404,
-  //   description:
-  //     'no inspiration - 데이터베이스에 글감이 없어서 나는 에러입니다.',
-  // })
-  // @Get('audio')
-  // async getAudio() {
-  //   try {
-  //     return await this.inspirationService.getAudio();
-  //   } catch (e) {
-  //     if (e instanceof Error) {
-  //       if (e.message === 'no inspiration') {
-  //         throw new HttpException('no inspiration', 404);
-  //       }
-  //     }
-  //     throw e;
-  //   }
-  // }
-
-  // @ApiOperation({ summary: '비디오 글감 받기 - 하루마다 랜덤으로 바뀜' })
-  // @ApiResponse({ status: 200, type: VideoDto })
-  // @ApiResponse({
-  //   status: 404,
-  //   description:
-  //     'no inspiration - 데이터베이스에 글감이 없어서 나는 에러입니다.',
-  // })
-  // @Get('video')
-  // async getVideo() {
-  //   try {
-  //     return await this.inspirationService.getVideo();
-  //   } catch (e) {
-  //     if (e instanceof Error) {
-  //       if (e.message === 'no inspiration') {
-  //         throw new HttpException('no inspiration', 404);
-  //       }
-  //     }
-  //     throw e;
-  //   }
-  // }
 }
